7c66799c10d947fab8455003404acdeb3dd62ab7
Peter Kieltyka <peter.kieltyka@nulayer.com>
2014-06-12 12:57:03 -0400

    Add example.sh

---
+++ example.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+##--
+## Example test script for QMD
+##--
+
+# CLI args
+username=$1
+force_err=$2
+
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <username> [force_err]"
+  echo "-"
+  echo "username : Your username!"
+  echo "force_err : Force the script to fail (for testing). Any value will make it err."
+  exit 1
+fi
+
+echo "Exec'ing $0 $@ in $QMD_TMP"
+
+if [ -n "$force_err" ]; then
+  echo "oops.. forced error....."
+  exit 1
+fi
+
+echo "Hello $username!"
+echo "<some return value.. for $username>" > $QMD_OUT
+exit 0
