382464496fe9e3185fe10e1fd6059034ab9a25ea
Vojtech Vitek (V-Teq) <vojtech.vitek@pressly.com>
2015-04-06 13:26:41 -0400

    Add Job implementation

--- job/job.go
+++ job/job.go
@@ -1,4 +1,61 @@
 package job
 
+import (
+	"io"
+	"os/exec"
+	"time"
+)
+
 type Job struct {
+	*exec.Cmd
+
+	Stdin   io.WriteCloser
+	Stdout  io.Reader
+	Stderr  io.Reader
+	Running bool
+	Start   time.Time
+	End     time.Time
+}
+
+func New(cmd *exec.Cmd) (*Job, error) {
+	var err error
+
+	job := &Job{
+		Cmd: cmd,
+	}
+
+	job.Stdout, err = cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	job.Stderr, err = cmd.StderrPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	job.Stdin, err = cmd.StdinPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	return job, nil
+}
+
+func (job *Job) Run() error {
+	if err := job.Cmd.Start(); err != nil {
+		return err
+	}
+
+	job.Running = true
+	return nil
+}
+
+func (job *Job) Wait() error {
+	if err := job.Cmd.Wait(); err != nil {
+		return err
+	}
+
+	job.Running = false
+	return nil
 }
---
+++ job/job_test.go
@@ -0,0 +1,49 @@
+package job
+
+import (
+	"bytes"
+	"os/exec"
+	"testing"
+)
+
+func TestRunJob(t *testing.T) {
+	// Bash command that prints "result" on STDOUT and "error" on STDERR.
+	cmd := exec.Command("bash", "-c", "echo -n result; >&2 echo -n error")
+
+	job, err := New(cmd)
+	if err != nil {
+		t.Error(err)
+	}
+	if job.Running {
+		t.Error("unexpected value")
+	}
+
+	// Copy job's STDOUT and STDERR to a buffer.
+	stdout, stderr := new(bytes.Buffer), new(bytes.Buffer)
+	go stdout.ReadFrom(job.Stdout)
+	go stderr.ReadFrom(job.Stderr)
+
+	err = job.Run()
+	if err != nil {
+		t.Error(err)
+	}
+	if !job.Running {
+		t.Error("unexpected value")
+	}
+
+	err = job.Wait()
+	if err != nil {
+		t.Error(err)
+	}
+	if job.Running {
+		t.Error("unexpected value")
+	}
+
+	// Test the job's STDOUT and STDERR.
+	if got := stdout.String(); got != "result" {
+		t.Errorf("unexpected stdout \"%s\"", got)
+	}
+	if got := stderr.String(); got != "error" {
+		t.Errorf("unexpected stderr \"%s\"", got)
+	}
+}
