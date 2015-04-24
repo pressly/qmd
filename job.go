153099a27307d9813fd7533dd8446b42f53e335a
Vojtech Vitek (V-Teq) <vojtech.vitek@pressly.com>
2015-04-24 15:29:14 -0400

    Fix "all the" race conditions

--- job.go
+++ job.go
@@ -19,28 +19,29 @@ import (
 type Job struct {
 	*exec.Cmd
 
-	ID    string
-	State JobState
-
+	ID          string
+	State       JobState
 	StartTime   time.Time
 	EndTime     time.Time
 	Duration    time.Duration
 	StatusCode  int
 	CallbackURL string
+	Err         error
 
 	StdoutFile        string
 	StderrFile        string
 	QmdOutFile        string
 	ExtraWorkDirFiles map[string]string
 
-	// Started channel blocks until the job is started.
+	// Started channel block until the job is started.
 	Started chan struct{}
-	// Finished channel blocks until the job is finished.
-	// Convinient with select{} statement with timeout.
+	// Finished channel block until the job is finished/killed/invalidated.
 	Finished chan struct{}
 
+	// WaitOnce guards the Wait() logic, so it can be called multiple times.
 	WaitOnce sync.Once
-	Err      error
+	// StartOnce guards the Start() logic, so it can be called multiple times.
+	StartOnce sync.Once
 }
 
 type JobState int
@@ -95,27 +96,31 @@ func (qmd *Qmd) Job(cmd *exec.Cmd) (*Job, error) {
 }
 
 func (job *Job) Start() error {
-	var err error
+	job.StartOnce.Do(job.startOnce)
 
-	if job.State >= Running {
-		return errors.New(fmt.Sprintf("/jobs/%v already running", job.ID))
-	}
+	// Wait for job to start.
+	<-job.Started
 
+	return job.Err
+}
+
+func (job *Job) startOnce() {
 	log.Printf("Starting /jobs/%v\n", job.ID)
 
+	var err error
 	job.Cmd.Stdout, err = os.Create(job.StdoutFile)
 	if err != nil {
-		return err
+		job.Err = err
 	}
 
 	job.Cmd.Stderr, err = os.Create(job.StderrFile)
 	if err != nil {
-		return err
+		job.Err = err
 	}
 
 	qmdOut, err := os.Create(job.QmdOutFile)
 	if err != nil {
-		return err
+		job.Err = err
 	}
 	qmdOut.Close()
 
@@ -137,7 +142,7 @@ func (job *Job) Start() error {
 
 	job.Cmd.SysProcAttr = &syscall.SysProcAttr{
 		Setpgid: true,
-		//Chroot: ,
+		//TODO: Chroot: job.Cmd.Dir,
 	}
 
 	if err := job.Cmd.Start(); err != nil {
@@ -148,15 +153,17 @@ func (job *Job) Start() error {
 	job.StartTime = time.Now()
 	job.State = Running
 	close(job.Started)
-	return nil
+	job.Err = nil
+	return
 
 failedToStart:
 	job.StatusCode = -1
 	job.State = Failed
+	job.WaitOnce.Do(func() {
+		close(job.Finished)
+	})
 	close(job.Started)
-	close(job.Finished)
 	log.Printf("Failed to start /jobs/%v: %v", job.ID, job.Err)
-	return job.Err
 }
 
 // Wait waits for job to finish.
@@ -165,43 +172,38 @@ func (job *Job) Wait() error {
 	// Wait for Start(), if not already invoked.
 	<-job.Started
 
-	if job.State != Running {
-		return job.Err
-	}
-
 	// Prevent running cmd.Wait() multiple times.
-	job.WaitOnce.Do(func() {
-		log.Printf("gonna wait")
-		err := job.Cmd.Wait()
-		log.Printf("wait done")
-		job.Duration = time.Since(job.StartTime)
-		job.EndTime = job.StartTime.Add(job.Duration)
-		if job.State != Terminated {
-			job.State = Finished
-		}
+	job.WaitOnce.Do(job.waitOnce)
 
-		if err != nil {
-			job.Err = err
-			if e, ok := err.(*exec.ExitError); ok {
-				if s, ok := e.Sys().(syscall.WaitStatus); ok {
-					job.StatusCode = s.ExitStatus()
-				}
-			}
-		}
-
-		log.Printf("wait done... but make sure all subprocesses are killed")
-		// Make sure to kill all the whole process group,
-		// so there are no subprocesses left.
-		job.Kill()
-
-		close(job.Finished)
-		log.Printf("/jobs/%v finished\n", job.ID)
-	})
-
-	// Make sure the job.WaitOnce finished.
+	// Wait for job to finish.
 	<-job.Finished
 
 	return job.Err
+}
+
+func (job *Job) waitOnce() {
+	err := job.Cmd.Wait()
+	job.Duration = time.Since(job.StartTime)
+	job.EndTime = job.StartTime.Add(job.Duration)
+	if job.State != Terminated {
+		job.State = Finished
+	}
+
+	if err != nil {
+		job.Err = err
+		if e, ok := err.(*exec.ExitError); ok {
+			if s, ok := e.Sys().(syscall.WaitStatus); ok {
+				job.StatusCode = s.ExitStatus()
+			}
+		}
+	}
+
+	// Make sure to kill the whole process group,
+	// so there are no subprocesses left.
+	job.Kill()
+
+	close(job.Finished)
+	log.Printf("/jobs/%v finished\n", job.ID)
 }
 
 func (job *Job) Run() error {
@@ -223,46 +225,45 @@ func (job *Job) Kill() error {
 	case Running:
 		job.State = Terminated
 		pgid, err := syscall.Getpgid(job.Cmd.Process.Pid)
-		if err == nil {
-			log.Printf("gonna killall")
-			// Kill the whole process group.
-			syscall.Kill(-pgid, 15)
-			log.Printf("killall done")
-		} else {
-			log.Printf("fall-back kill")
+		if err != nil {
 			// Fall-back on error. Kill the main process only.
 			job.Cmd.Process.Kill()
-			log.Printf("fall-back kill done")
+			break
 		}
+		// Kill the whole process group.
+		syscall.Kill(-pgid, 15)
 
 	case Finished:
-		// Make sure to kill all the whole process group,
-		// so there are no subprocesses left.
 		pgid, err := syscall.Getpgid(job.Cmd.Process.Pid)
 		if err != nil {
 			break
 		}
-		log.Printf("gonna killall")
-		// Kill the whole process group.
+		// Make sure to kill the whole process group,
+		// so there are no subprocesses left.
 		syscall.Kill(-pgid, 15)
-		log.Printf("killall done")
 
 	case Initialized, Enqueued:
-		job.State = Invalidated
-		job.WaitOnce.Do(func() {
-			close(job.Finished)
+		// This one is tricky, as the job's Start() might have
+		// been called and is already in progress, but the job's
+		// state is not Running yet.
+		usCallingStartOnce := false
+		job.StartOnce.Do(func() {
+			job.WaitOnce.Do(func() {
+				job.State = Invalidated
+				job.StatusCode = -2
+				job.Err = errors.New("invalidated")
+				log.Printf("Invalidating /jobs/%v\n", job.ID)
+				close(job.Finished)
+			})
+			close(job.Started)
+			usCallingStartOnce = true
 		})
-		close(job.Started)
-		job.StatusCode = -2
-		job.Err = errors.New("invalidated")
-		log.Printf("Invalidating /jobs/%v\n", job.ID)
-
-	case Terminated:
-		// NOP. The job might have been killed already
-		// or it failed to start.
-		log.Printf("Kill(): Job %v already Terminated, PID %v state: %v", job.ID, job.Cmd.Process.Pid, job.Cmd.ProcessState)
-	default:
-		// NOP.
+		if !usCallingStartOnce {
+			// It was job.Start() that called StartOnce.Do(), not us,
+			// thus we need to wait for Started and try to Kill again:
+			<-job.Started
+			job.Kill()
+		}
 	}
 
 	return job.Err
@@ -279,9 +280,9 @@ func (s JobState) String() string {
 	case Finished:
 		return "Finished"
 	case Terminated:
-		return "Terminated"
+		return "Terminated (killed by us)"
 	case Invalidated:
-		return "Terminated before start"
+		return "Invalidated before start"
 	case Failed:
 		return "Failed to start"
 	}
--- queue.go
+++ queue.go
@@ -20,26 +20,18 @@ func (qmd *Qmd) startWorker(id int, workerPool chan chan *Job /*, quitWorkerPool
 		select {
 		// Wait for a job.
 		case job := <-worker:
-			log.Printf("Worker[%v] going to run job..\n", id)
 			// Run a job.
 			go job.Run()
-			log.Printf("Worker[%v] Run()..\n", id)
 			<-job.Started
-
-			log.Printf("Worker[%v] <-Started..\n", id)
 
 			select {
 			// Wait for the job to finish.
 			case <-job.Finished:
-				log.Printf("Worker[%v] <-Finished..\n", id)
 
 			// Or kill it, if it doesn't finish in a specified time.
 			case <-time.After(time.Duration(qmd.Config.MaxExecTime) * time.Second):
-				log.Printf("Worker[%v] <-Timeout... Kill()\n", id)
 				job.Kill()
-				log.Printf("Worker[%v] Wait()\n", id)
 				job.Wait()
-				log.Printf("Worker[%v] done..\n", id)
 
 				// case <-quit:
 				// 	log.Printf("worker[%d]: Stopping\n", id)
@@ -62,7 +54,6 @@ func (qmd *Qmd) ListenQueue() {
 		select {
 		// Wait for some worker to become available.
 		case worker := <-workerPool:
-			log.Printf("Worker ready for new job..\n")
 			// Send it a job.
 			job := <-qmd.Queue
 			worker <- job
