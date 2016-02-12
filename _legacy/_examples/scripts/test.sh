62be81c81d180012be55df939502001eaf9b4c8c
Vojtech Vitek (V-Teq) <vojtech.vitek@pressly.com>
2016-02-11 19:07:56 -0500

    Remove legacy code

--- _legacy/_examples/scripts/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Hello world"
-echo $QMD_TMP
-echo $QMD_STORE
-echo $QMD_OUT
--- _legacy/_examples/scripts/wait.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-WAITTIME=$1
-echo "Hi, I'm sleeping for $WAITTIME seconds..."
-sleep $WAITTIME
-echo "All done!"
